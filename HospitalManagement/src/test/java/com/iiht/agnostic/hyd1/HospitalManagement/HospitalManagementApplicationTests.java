d7616936d3b81c1d42d5ccdef8b4c70a526b9562
ADMIN <ADMIN@Lenovo-PC>
2019-10-29 17:11:34 +0530

    First Commit by Rani

---
+++ HospitalManagement/src/test/java/com/iiht/agnostic/hyd1/HospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiht.agnostic.hyd1.HospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
